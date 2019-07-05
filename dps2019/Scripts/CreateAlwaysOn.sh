0152ab90d166da4d7ebf1ecca578ff97ff044863
Miguel Egea <34855760+MegeaPortalSQL@users.noreply.github.com>
2019-07-05 09:33:21 +0200

    Update CreateAlwaysOn.sh

--- dps2019/Scripts/CreateAlwaysOn.sh
+++ dps2019/Scripts/CreateAlwaysOn.sh
@@ -1,8 +1,8 @@
 
-sqlcmd -S db1.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 01_PrincipalServer.sql
-sqlcmd -S db2.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 02_SecondaryServer.sql
-sqlcmd -S db3.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 02_SecondaryServer.sql
-sqlcmd -S db1.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 03_CreateAvailabilityGroup.sql
-sqlcmd -S db2.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 04_JoinAvailabilityGroup.sql
-sqlcmd -S db3.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 04_JoinAvailabilityGroup.sql
+sqlcmd -S db1.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 01_PrincipalServer.sql
+sqlcmd -S db2.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 02_SecondaryServer.sql
+sqlcmd -S db3.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 02_SecondaryServer.sql
+sqlcmd -S db1.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 03_CreateAvailabilityGroup.sql
+sqlcmd -S db2.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 04_JoinAvailabilityGroup.sql
+sqlcmd -S db3.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 04_JoinAvailabilityGroup.sql
 
