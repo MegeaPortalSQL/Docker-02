9e262d37ffb0225ecfa6c8b2d7d4542015d011f8
Miguel Egea <34855760+MegeaPortalSQL@users.noreply.github.com>
2019-07-12 11:14:51 +0200

    Added control of instance created
    This change allow us to create the entire enviroment just executing docker-compose build + docker-compose up

--- dps2019/Scripts/CreateAlwaysOn.sh
+++ dps2019/Scripts/CreateAlwaysOn.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+# wait for MSSQL server to start
+export STATUS=1
+i=0
+
+while [[ $STATUS -ne 0 ]] && [[ $i -lt 30 ]]; do
+	i=$i+1
+	/opt/mssql-tools/bin/sqlcmd -t 1 -U sa -P $MSSQL_SA_PASSWORD -Q "select 1" >> /dev/null
+	STATUS=$?
+done
+
+if [ $STATUS -ne 0 ]; then 
+	echo "Error: MSSQL SERVER took more than thirty seconds to start up."
+	exit 1
+fi
+
+echo "======= MSSQL SERVER STARTED ========" 
+
 
 sqlcmd -S db1.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 01_PrincipalServer.sql
 sqlcmd -S db2.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 02_SecondaryServer.sql
@@ -5,4 +24,5 @@ sqlcmd -S db3.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 02_SecondaryS
 sqlcmd -S db1.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 03_CreateAvailabilityGroup.sql
 sqlcmd -S db2.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 04_JoinAvailabilityGroup.sql
 sqlcmd -S db3.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i 04_JoinAvailabilityGroup.sql
+sqlcmd -S db1.internal.portalsql.es -U SA -P 'PortalSQL01Demo#' -i RestoreAdventureWorksDWandPutitAvailable.sql
 
